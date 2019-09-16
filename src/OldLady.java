4823f03931b5874892451e990168c4c277250c2f
primavio000 <primavio000@CAM-LAB-22.bgsd.k12.wa.us>
2019-09-16 07:41:10 -0700

    Save

--- src/OldLady.java
+++ src/OldLady.java
@@ -2,8 +2,67 @@
 public class OldLady {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		fly();
+		idky();
+		spider();
+		swalSpid();
+		idky();
+		bird();
+		swalBird();
+		swalSpid();
+		idky();
+		cat();
+		swalCat();
+		swalBird();
+		swalSpid();
+		idky();
+		dog();
+		swalCat();
+		swalBird();
+		swalSpid();
+		idky();
+		horse();
 	}
 
+	public static void fly() {
+		System.out.println("There was an old lady who swallowed a fly.");
+	}
+	
+	public static void idky() {
+		System.out.println("I don't know why she swallowed that fly,\nPerhaps she'll die.");
+		System.out.println("");
+		}
+		
+	public static void spider()  {
+		System.out.println("There was an old lady who swallowed a spider,\nThat wriggled and iggled and jiggled inside her.");
+	}
+	
+	public static void swalSpid() {
+		System.out.println("She swallowed the spider to catch the fly,");
+	}
+	
+	public static void bird() {
+		System.out.println("There was an old lady who swallowed a bird,\nHow absurd to swallow a bird.");
+	}
+	
+	public static void swalBird() {
+		System.out.println("She swallowed the bird to catch the spider,");
+	}
+	
+	public static void dog() {
+		System.out.println("There was an old lady who swallowed a dog,\nWhat a hog to swallow a dog.");
+		System.out.println("She swallowed the dog to catch the cat,");
+	}
+	
+	public static void horse() {
+		System.out.println("There was an old lady who swallowd a horse,\nShe died of course.");
+	}
+	
+	public static void cat() {
+		System.out.println("There was an old lady who swallowed a cat,\nImagine that to swallow a cat");
+	}
+	
+	public static void swalCat() {
+		System.out.println("She swallowed the cat to catch the bird,");
+	}
 }
